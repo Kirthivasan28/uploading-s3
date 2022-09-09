efdd5e9b192de4533ca0443c5d4ff31921a3400b
Kirthivasan28 <kirthivasan.cp@vuedata.in>
2022-09-09 15:48:37 +0530

    changed folder

--- scripts/pipeline/release.sh
+++ scripts/pipeline/release.sh
@@ -8,7 +8,7 @@ local_path=$5
 rm -rf $local_path
 
 # Create a zip of the current directory.
-zip -r $local_path . -x main.py
+zip -r $local_path . -x .packages/
 
 # Install required dependencies for Python script.
 pip3 install boto3
