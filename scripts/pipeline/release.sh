35a951cd41497ba46bceb54a3210749d338a0180
Kirthivasan28 <kirthivasan.cp@vuedata.in>
2022-09-13 12:27:49 +0530

    kooo

--- scripts/pipeline/release.sh
+++ scripts/pipeline/release.sh
@@ -7,11 +7,17 @@ local_path=$5
 # Remove any existing versions of a ZIP
 rm -rf $local_path
 
+
+
 # Create a zip of the current directory.
-zip -r $local_path packages/ 
+zip -r $local_path packages/test1/ 
+
+zip -r $local_path packages/test2/
 
 # Install required dependencies for Python script.
 pip3 install boto3
 
 # Run upload script
+python3 scripts/pipeline/upload_file_to_s3.py $bucket_name $aws_key $aws_access_key $aws_access_secret $local_path
+
 python3 scripts/pipeline/upload_file_to_s3.py $bucket_name $aws_key $aws_access_key $aws_access_secret $local_path
