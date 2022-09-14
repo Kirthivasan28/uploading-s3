2070eff4454f88cd55ff5eb64b14b3e8f9ccadd7
Kirthivasan28 <kirthivasan.cp@vuedata.in>
2022-09-14 17:50:27 +0530

    sadf

--- scripts/pipeline/release.sh
+++ scripts/pipeline/release.sh
@@ -17,4 +17,4 @@ python3 scripts/pipeline/upload_file_to_s3.py $bucket_name $aws_key $aws_access_
 
 zip -r $local_path packages/test2
 
-
+python3 scripts/pipeline/upload_file_to_s3.py $bucket_name $aws_key $aws_access_key $aws_access_secret $local_path
