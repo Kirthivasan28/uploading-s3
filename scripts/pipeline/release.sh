796026e5916c2827297af42cbca38e4118323cfe
Kirthivasan28 <kirthivasan.cp@vuedata.in>
2022-09-14 17:11:26 +0530

    looo

--- scripts/pipeline/release.sh
+++ scripts/pipeline/release.sh
@@ -11,6 +11,7 @@ zip -r $local_path packages/test2
 # Install required dependencies for Python script.
 pip3 install boto3
 echo $local_path
+echo "$kirthii"
 # Run upload script
 python3 scripts/pipeline/upload_file_to_s3.py $bucket_name $aws_key $aws_access_key $aws_access_secret $local_path
 
