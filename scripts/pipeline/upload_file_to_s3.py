424bb8d83b0ddacf1fc89561d2f1016851286e3f
Kirthivasan28 <kirthivasan.cp@vuedata.in>
2022-09-14 14:33:58 +0530

    last

--- scripts/pipeline/upload_file_to_s3.py
+++ scripts/pipeline/upload_file_to_s3.py
@@ -2,8 +2,7 @@ import boto3
 import sys
 
 def main():
-    print(len(sys.argv),'len of system argument')
-    if (len(sys.argv) < 7):
+    if (len(sys.argv) < 6):
         print ('Error: Required 5 arguments.')
         # Checks for 6 because the script path is in position 0. So len is 6
         # for 5 arguments.
@@ -14,7 +13,7 @@ def main():
     aws_access_key=sys.argv[3]
     aws_access_secret=sys.argv[4]
     local_path=sys.argv[5]
-    print(sys.argv[5],'file path')
+
     session = boto3.Session(
         aws_access_key_id=aws_access_key,
         aws_secret_access_key=aws_access_secret,
@@ -27,7 +26,6 @@ def main():
         Key=aws_key
     )
     print ('Done uploading')
-    # print(response['Filename'],'response value...')
-    print(dir(response),'directory response')
+    print(local_path,'local path value')
 
 main()
